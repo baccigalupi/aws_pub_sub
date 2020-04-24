b8436bd31d82df23b85585fe3556de09649200ab
Kane Baccigalupi <baccigalupi@gmail.com>
2020-04-24 15:01:59 -0700

    Gets the generator script running (from here)!

--- lib/aws_pub_sub/generate.rb
+++ lib/aws_pub_sub/generate.rb
@@ -1,3 +1,29 @@
-require "fileutils"
+module AwsPubSub
+  class Generate
+    attr_reader :path
 
-require_relative "generate/runner"
+    def initialize(path)
+      @path = File.expand_path(
+        File.join(Dir.pwd, path)
+      )
+    end
+
+    def run
+      copy_templates
+      mv_gitignore
+    end
+
+    def copy_templates
+      FileUtils.mkdir_p(path)
+      Dir["#{template_dir}/*"].each{ |f| FileUtils.cp_r(f, path) }
+    end
+
+    def mv_gitignore
+      FileUtils.mv("#{path}/gitignore", "#{path}/.gitignore")
+    end
+
+    def template_dir
+      File.expand_path(File.join(__dir__, "app_template"))
+    end
+  end
+end
